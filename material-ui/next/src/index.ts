502f10bc4222ae8ad9b0dd099d5d95213001f3ac
Takeshi Iwana <tak@prismify.co>
2020-10-08 13:34:51 -0600

    Chain program and add missing identifier

--- material-ui/next/src/index.ts
+++ material-ui/next/src/index.ts
@@ -86,130 +86,137 @@ export default builder()
             )
           }
         })
-      program.find(j.ImportDeclaration).forEach((importPath) => {
-        if (
-          !importPath.value.specifiers
-            .filter((spec) => j.ImportSpecifier.check(spec))
-            .some(
-              (node) =>
-                (node as j.ImportSpecifier)?.imported?.name ===
-                'DocumentContext'
+        .find(j.ImportDeclaration)
+        .forEach((importPath) => {
+          if (
+            !importPath.value.specifiers
+              .filter((spec) => j.ImportSpecifier.check(spec))
+              .some(
+                (node) =>
+                  (node as j.ImportSpecifier)?.imported?.name ===
+                  'DocumentContext'
+              )
+          ) {
+            importPath.value.specifiers.splice(
+              0,
+              0,
+              j.importSpecifier(j.identifier('DocumentContext'))
             )
-        ) {
-          importPath.value.specifiers.splice(
-            0,
-            0,
-            j.importSpecifier(j.identifier('DocumentContext'))
-          )
-        }
-      })
-
-      program.find(j.MethodDefinition).forEach((path) => {
-        if ((path.value.key as any).name !== 'getInitialProps') return
-        const body = j(path).find(j.BlockStatement).get('body')
-        const [variableDeclaration] = body.value
-        const sheets = j.variableDeclaration('const', [
-          j.variableDeclarator(
-            j.identifier('sheets'),
-            j.newExpression(j.identifier('ServerStyleSheets'), [])
-          ),
-        ])
-
-        const originalRenderPage = j.variableDeclaration('const', [
-          j.variableDeclarator(
-            j.identifier('originalRenderPage'),
-            j.memberExpression(j.identifier('ctx'), j.identifier('renderPage'))
-          ),
-        ])
-
-        const ctxRenderPage = j.expressionStatement(
-          j.assignmentExpression(
-            '=',
-            j.memberExpression(j.identifier('ctx'), j.identifier('renderPage')),
-            j.arrowFunctionExpression(
-              [],
-              j.callExpression(j.identifier('originalRenderPage'), [
-                j.objectExpression([
-                  j.objectProperty(
-                    j.identifier('enhanceApp'),
-                    j.arrowFunctionExpression(
-                      [j.identifier('App')],
-                      j.arrowFunctionExpression(
-                        [j.identifier('props')],
-                        j.callExpression(
-                          j.memberExpression(
-                            j.identifier('sheets'),
-                            j.identifier('collect')
-                          ),
-                          [
-                            j.jsxElement(
-                              j.jsxOpeningElement(
-                                j.jsxIdentifier('App'),
-                                [j.jsxSpreadAttribute(j.identifier('props'))],
-                                true
-                              )
-                            ),
-                          ]
-                        )
-                      )
-                    )
-                  ),
-                ]),
-              ])
-            )
-          )
-        )
-
-        const returnStatement = j.returnStatement(
-          j.objectExpression([
-            j.spreadElement(j.identifier('initialProps')),
-            j.objectProperty(
-              j.identifier('styles'),
-              j.arrayExpression([
-                j.spreadElement(
-                  j.callExpression(
-                    j.memberExpression(
-                      j.memberExpression(
-                        j.identifier('React'),
-                        j.identifier('Children')
-                      ),
-                      j.identifier('toArray')
-                    ),
-                    [
-                      j.memberExpression(
-                        j.identifier('initialProps'),
-                        j.identifier('styles')
-                      ),
-                    ]
-                  )
-                ),
-                j.callExpression(
-                  j.memberExpression(
-                    j.identifier('sheets'),
-                    j.identifier('getStyleElement')
-                  ),
-                  []
-                ),
-              ])
+          }
+        })
+        .find(j.MethodDefinition)
+        .forEach((path) => {
+          if ((path.value.key as any).name !== 'getInitialProps') return
+          const body = j(path).find(j.BlockStatement).get('body')
+          const [variableDeclaration] = body.value
+          const sheets = j.variableDeclaration('const', [
+            j.variableDeclarator(
+              j.identifier('sheets'),
+              j.newExpression(j.identifier('ServerStyleSheets'), [])
             ),
           ])
-        )
 
-        j(path).replaceWith(
-          j.classMethod(
-            'method',
-            j.identifier('getIntialProps'),
-            [],
-            j.blockStatement([
-              sheets,
-              originalRenderPage,
-              variableDeclaration,
-              ctxRenderPage,
-              returnStatement,
+          const originalRenderPage = j.variableDeclaration('const', [
+            j.variableDeclarator(
+              j.identifier('originalRenderPage'),
+              j.memberExpression(
+                j.identifier('ctx'),
+                j.identifier('renderPage')
+              )
+            ),
+          ])
+
+          const ctxRenderPage = j.expressionStatement(
+            j.assignmentExpression(
+              '=',
+              j.memberExpression(
+                j.identifier('ctx'),
+                j.identifier('renderPage')
+              ),
+              j.arrowFunctionExpression(
+                [],
+                j.callExpression(j.identifier('originalRenderPage'), [
+                  j.objectExpression([
+                    j.objectProperty(
+                      j.identifier('enhanceApp'),
+                      j.arrowFunctionExpression(
+                        [j.identifier('App')],
+                        j.arrowFunctionExpression(
+                          [j.identifier('props')],
+                          j.callExpression(
+                            j.memberExpression(
+                              j.identifier('sheets'),
+                              j.identifier('collect')
+                            ),
+                            [
+                              j.jsxElement(
+                                j.jsxOpeningElement(
+                                  j.jsxIdentifier('App'),
+                                  [j.jsxSpreadAttribute(j.identifier('props'))],
+                                  true
+                                )
+                              ),
+                            ]
+                          )
+                        )
+                      )
+                    ),
+                  ]),
+                ])
+              )
+            )
+          )
+
+          const returnStatement = j.returnStatement(
+            j.objectExpression([
+              j.spreadElement(j.identifier('initialProps')),
+              j.objectProperty(
+                j.identifier('styles'),
+                j.arrayExpression([
+                  j.spreadElement(
+                    j.callExpression(
+                      j.memberExpression(
+                        j.memberExpression(
+                          j.identifier('React'),
+                          j.identifier('Children')
+                        ),
+                        j.identifier('toArray')
+                      ),
+                      [
+                        j.memberExpression(
+                          j.identifier('initialProps'),
+                          j.identifier('styles')
+                        ),
+                      ]
+                    )
+                  ),
+                  j.callExpression(
+                    j.memberExpression(
+                      j.identifier('sheets'),
+                      j.identifier('getStyleElement')
+                    ),
+                    []
+                  ),
+                ])
+              ),
             ])
           )
-        )
-      })
+
+          j(path).replaceWith(
+            j.classMethod(
+              'method',
+              j.identifier('getIntialProps'),
+              [j.identifier('ctx')],
+              j.blockStatement([
+                sheets,
+                originalRenderPage,
+                variableDeclaration,
+                ctxRenderPage,
+                returnStatement,
+              ])
+            )
+          )
+        })
 
       // import React if it wasn't already imported
       if (!isReactImported) {
