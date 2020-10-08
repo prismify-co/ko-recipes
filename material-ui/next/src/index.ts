e569fb355633b34252dffbeb4fdcab6760049e98
Takeshi Iwana <tak@prismify.co>
2020-10-08 13:21:20 -0600

    Fix material-ui codemod for next

--- material-ui/next/src/index.ts
+++ material-ui/next/src/index.ts
@@ -86,152 +86,129 @@ export default builder()
             )
           }
         })
-      program
-        .find(j.ImportDeclaration, { source: { value: 'blitz' } })
-        .forEach((blitzImportPath) => {
-          if (
-            !blitzImportPath.value.specifiers
-              .filter((spec) => j.ImportSpecifier.check(spec))
-              .some(
-                (node) =>
-                  (node as j.ImportSpecifier)?.imported?.name ===
-                  'DocumentContext'
-              )
-          ) {
-            blitzImportPath.value.specifiers.splice(
-              0,
-              0,
-              j.importSpecifier(j.identifier('DocumentContext'))
+      program.find(j.ImportDeclaration).forEach((importPath) => {
+        if (
+          !importPath.value.specifiers
+            .filter((spec) => j.ImportSpecifier.check(spec))
+            .some(
+              (node) =>
+                (node as j.ImportSpecifier)?.imported?.name ===
+                'DocumentContext'
             )
-          }
-        })
-      program.find(j.ClassBody).forEach((path) => {
-        const { node } = path
+        ) {
+          importPath.value.specifiers.splice(
+            0,
+            0,
+            j.importSpecifier(j.identifier('DocumentContext'))
+          )
+        }
+      })
 
-        const ctxParam = j.identifier('ctx')
-        ctxParam.typeAnnotation = j.tsTypeAnnotation(
-          j.tsTypeReference(j.identifier('DocumentContext'))
-        )
-
-        const getInitialPropsBody = j.blockStatement([
-          j.variableDeclaration('const', [
-            j.variableDeclarator(
-              j.identifier('sheets'),
-              j.newExpression(j.identifier('ServerStyleSheets'), [])
-            ),
-          ]),
-          j.variableDeclaration('const', [
-            j.variableDeclarator(
-              j.identifier('originalRenderPage'),
-              j.memberExpression(
-                j.identifier('ctx'),
-                j.identifier('renderPage')
-              )
-            ),
-          ]),
-          j.expressionStatement(
-            j.assignmentExpression(
-              '=',
-              j.memberExpression(
-                j.identifier('ctx'),
-                j.identifier('renderPage')
-              ),
-              j.arrowFunctionExpression(
-                [],
-                j.callExpression(j.identifier('originalRenderPage'), [
-                  j.objectExpression([
-                    j.objectProperty(
-                      j.identifier('enhanceApp'),
-                      j.arrowFunctionExpression(
-                        [j.identifier('App')],
-                        j.arrowFunctionExpression(
-                          [j.identifier('props')],
-                          j.callExpression(
-                            j.memberExpression(
-                              j.identifier('sheets'),
-                              j.identifier('collect')
-                            ),
-                            [
-                              j.jsxElement(
-                                j.jsxOpeningElement(
-                                  j.jsxIdentifier('App'),
-                                  [j.jsxSpreadAttribute(j.identifier('props'))],
-                                  true
-                                )
-                              ),
-                            ]
-                          )
-                        )
-                      )
-                    ),
-                  ]),
-                ])
-              )
-            )
-          ),
-          j.variableDeclaration('const', [
-            j.variableDeclarator(
-              j.identifier('initialProps'),
-              j.awaitExpression(
-                j.callExpression(
-                  j.memberExpression(
-                    j.identifier('Document'),
-                    j.identifier('getInitialProps')
-                  ),
-                  [j.identifier('ctx')]
-                )
-              )
-            ),
-          ]),
-          j.returnStatement(
-            j.objectExpression([
-              j.spreadElement(j.identifier('initialProps')),
-              j.objectProperty(
-                j.identifier('styles'),
-                j.arrayExpression([
-                  j.spreadElement(
-                    j.callExpression(
-                      j.memberExpression(
-                        j.memberExpression(
-                          j.identifier('React'),
-                          j.identifier('Children')
-                        ),
-                        j.identifier('toArray')
-                      ),
-                      [
-                        j.memberExpression(
-                          j.identifier('initialProps'),
-                          j.identifier('styles')
-                        ),
-                      ]
-                    )
-                  ),
-                  j.callExpression(
-                    j.memberExpression(
-                      j.identifier('sheets'),
-                      j.identifier('getStyleElement')
-                    ),
-                    []
-                  ),
-                ])
-              ),
-            ])
+      program.find(j.MethodDefinition).forEach((path) => {
+        if ((path.value.key as any).name !== 'getInitialProps') return
+        const body = j(path).find(j.BlockStatement).get('body')
+        const [variableDeclaration] = body.value
+        const sheets = j.variableDeclaration('const', [
+          j.variableDeclarator(
+            j.identifier('sheets'),
+            j.newExpression(j.identifier('ServerStyleSheets'), [])
           ),
         ])
 
-        const getInitialPropsMethod = j.classMethod(
-          'method',
-          j.identifier('getInitialProps'),
-          [ctxParam],
-          getInitialPropsBody,
-          false,
-          true
-        )
-        getInitialPropsMethod.async = true
+        const originalRenderPage = j.variableDeclaration('const', [
+          j.variableDeclarator(
+            j.identifier('originalRenderPage'),
+            j.memberExpression(j.identifier('ctx'), j.identifier('renderPage'))
+          ),
+        ])
 
-        // TODO: better way will be to check if the method already exists and modify it or else add it
-        // currently it gets added assuming it did not exist before
-        node.body.splice(0, 0, getInitialPropsMethod)
+        const ctxRenderPage = j.expressionStatement(
+          j.assignmentExpression(
+            '=',
+            j.memberExpression(j.identifier('ctx'), j.identifier('renderPage')),
+            j.arrowFunctionExpression(
+              [],
+              j.callExpression(j.identifier('originalRenderPage'), [
+                j.objectExpression([
+                  j.objectProperty(
+                    j.identifier('enhanceApp'),
+                    j.arrowFunctionExpression(
+                      [j.identifier('App')],
+                      j.arrowFunctionExpression(
+                        [j.identifier('props')],
+                        j.callExpression(
+                          j.memberExpression(
+                            j.identifier('sheets'),
+                            j.identifier('collect')
+                          ),
+                          [
+                            j.jsxElement(
+                              j.jsxOpeningElement(
+                                j.jsxIdentifier('App'),
+                                [j.jsxSpreadAttribute(j.identifier('props'))],
+                                true
+                              )
+                            ),
+                          ]
+                        )
+                      )
+                    )
+                  ),
+                ]),
+              ])
+            )
+          )
+        )
+
+        const returnStatement = j.returnStatement(
+          j.objectExpression([
+            j.spreadElement(j.identifier('initialProps')),
+            j.objectProperty(
+              j.identifier('styles'),
+              j.arrayExpression([
+                j.spreadElement(
+                  j.callExpression(
+                    j.memberExpression(
+                      j.memberExpression(
+                        j.identifier('React'),
+                        j.identifier('Children')
+                      ),
+                      j.identifier('toArray')
+                    ),
+                    [
+                      j.memberExpression(
+                        j.identifier('initialProps'),
+                        j.identifier('styles')
+                      ),
+                    ]
+                  )
+                ),
+                j.callExpression(
+                  j.memberExpression(
+                    j.identifier('sheets'),
+                    j.identifier('getStyleElement')
+                  ),
+                  []
+                ),
+              ])
+            ),
+          ])
+        )
+
+        j(path).replaceWith(
+          j.classMethod(
+            'method',
+            j.identifier('getIntialProps'),
+            [],
+            j.blockStatement([
+              sheets,
+              originalRenderPage,
+              variableDeclaration,
+              ctxRenderPage,
+              returnStatement,
+            ])
+          )
+        )
       })
 
       // import React if it wasn't already imported
